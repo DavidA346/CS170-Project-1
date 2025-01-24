c6e3505e16b7fcee66690a23076b174173125db0
DavidA346 <davidaispuro346@gmail.com>
2025-01-23 16:51:33 -0800

    Added the structure for UCS, just missing expanding the children

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 //Priority queue data structure that enqueues nodes in order of cost
@@ -15,17 +16,81 @@ using namespace std;
     //if tie between costs choose first (does not matter
     //Return path to goal state and total cost
 
+//Node struct that encapsulates all the necessary info for a node
+struct Node {
+    int puzzle[3][3];
+
+    //Constructor that sets the node's puzzle to given puzzle
+    Node(int currentPuzzle[3][3]) {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j){
+                puzzle[i][j] = currentPuzzle[i][j];
+            }
+        }
+    }
+};
+
+//Helper function that checks if goal is reached
+bool isGoalState(Node* currentState) {
+    int goalState[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 0}
+    };
+
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j){
+            if (goalState[i][j] != currentState->puzzle[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+//Helper function that creates the children of the current state
+// vector<Node*> createChildren(Node* currentState) {
+
+// }
+
 //Uniform Cost Search Algorithm
+void uniformCostSearch(Node* &problem){
     //1.Create priority queue
-    //2. Enter while loop to see if we reach goal state
+    priority_queue<Node*> nodes;
+    //Create vector of visited nodes to prevent repeat states
+    vector<Node*> visited;
+
+    //Push root/initial state to priority queue
+    nodes.push(problem);
+
+    //2. Enter while loop to see if we reach goal state and keep looping until nodes is empty
+    while (!nodes.empty()) {
         //3.If priority queue is empty then return 'No solution'
-        //4.Pop the head node
-        //4.If current node is goal node return total cost and exit search
-        //5.Else enqueue all the children of current node with total cost from current node and add current node to visited list
-        
+        if (nodes.empty()) {
+            cout << "No solution found" << endl;
+        }
+
+        //4. Pop the head node
+        Node* currentNode = nodes.top();
+        visited.push_back(currentNode);
+        nodes.pop();
+
+        //5. If current node is goal node return total cost and exit search
+        if (isGoalState(currentNode)) {
+            cout << "DONE" << endl;
+        }
+
+        //6. Else enqueue all the children of current node with total cost from current node and add current node to visited list
+        else {
+            //TODO: Create children and enqueue them
+            cout << "need to enqueue children" << endl;
+        }
+    }
+}
+
 int main() {
     //Puzzle user enters
-    vector<int> userPuzzle;
+    int userPuzzle[3][3];
 
     //Prompt user to enter a puzzle
     cout << "Welcome to the 8 puzzle!!!" << endl;
@@ -33,11 +98,16 @@ int main() {
     cout << "Make sure to enter the eight numbers in any order and enter a '0' to denote the blank tile: " << endl;
 
     //Push into vector user puzzle
-    for (int i = 0; i < 9; ++i) {
-        int input;
-        cin >> input;
-        userPuzzle.push_back(input);
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            int input;
+            cin >> input;
+            userPuzzle[i][j] = input;
+        }
     }
+
+    Node* problem = new Node(userPuzzle);
+    uniformCostSearch(problem);
 
     return 0;
 }
